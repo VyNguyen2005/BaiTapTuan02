3f4339760db69c30f74660c0a72270a93160b984
ADMIN <ADMIN@PC315>
2024-08-23 16:48:48 +0700

    "version 2"

--- src/main/java/lab02_BT03_KeThua/Program.java
+++ src/main/java/lab02_BT03_KeThua/Program.java
@@ -5,47 +5,102 @@
 package lab02_BT03_KeThua;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author nguye
  */
 public class Program {
+    static ArrayList<SinhVien> danhsachSinhVien = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("---Danh sach sinh vien IT---");
-        ArrayList<SinhVienIT> danhSachSinhVienIT = new ArrayList<>();
-        
-        danhSachSinhVienIT.add(new SinhVienIT("Nguyen Van A", 8, 9, 10));
-        danhSachSinhVienIT.add(new SinhVienIT("Tran Thi B", 7.5, 8, 9));
-        danhSachSinhVienIT.add(new SinhVienIT("Le Van C", 6.5, 7, 8));
-        danhSachSinhVienIT.add(new SinhVienIT("Pham Thi D", 3, 4, 2));
-        
-        for (SinhVienIT sinhVienIT : danhSachSinhVienIT) {
-            sinhVienIT.xuat();
-        }
-        
-        System.out.println("---Danh sach sinh vien Biz---");
-        ArrayList<SinhVienBiz> danhSachSinhVienBiz = new ArrayList<>();
-        
-        danhSachSinhVienBiz.add(new SinhVienBiz("Ha Thi A", 5, 4));
-        danhSachSinhVienBiz.add(new SinhVienBiz("Nguyen Thi B", 7.5, 6));
-        danhSachSinhVienBiz.add(new SinhVienBiz("Mai Van E", 8, 8.5));
-        danhSachSinhVienBiz.add(new SinhVienBiz("Hoang Xuan D", 9, 10));
-        
-        for (SinhVienBiz sinhVienBiz : danhSachSinhVienBiz) {
-            sinhVienBiz.xuat();
-        }
-        
-        System.out.println("---Danh sach sinh vien co hoc luc GIOI---");
-        for (SinhVienBiz sinhVienBiz : danhSachSinhVienBiz) {
-            if(sinhVienBiz.getDiem() < 9 && sinhVienBiz.getDiem() >= 7.5){
-                sinhVienBiz.xuat();
+        Menu();
+    }
+
+    private static void Menu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("CHUONG TRINH QUAN LI SINH VIEN");
+        System.out.println("-------------------------------");
+        System.out.println("1. Nhap danh sach sinh vien");
+        System.out.println("2. Xuat thong tin danh sach sinh vien");
+        System.out.println("3. Xuat danh sach sinh vien co hoc luc gioi");
+        System.out.println("4. Sap xep danh sach sinh vien theo diem");
+        System.out.println("5. Ket thuc");
+        System.out.println("-------------------------------");
+        System.out.print("Nhap yeu cau cua ban: ");
+        int luachon = sc.nextInt();
+        do {
+            switch (luachon) {
+                case 1:
+                    NhapDS();
+                    break;
+                case 2:
+                    XuatDS();
+                    break;
+                case 3:
+                    XuatDSGioi();
+                    break;
+                case 4:
+                    SapXepTheoDiem();
+                    break;
+                default:
+                    System.out.println("Ket thuc chuong trinh!!!");
+                    break;
             }
+        } while (luachon != 5);
+    }
+    public static void NhapDS() {
+        String tieptuc;
+        do {
+            SinhVien sv = null;
+            Scanner sc1 = new Scanner(System.in);
+            Scanner sc2 = new Scanner(System.in);
+            System.out.println("Thuc hien nhap danh sach sinh vien IT va Biz");
+            System.out.print("Ban muon nhap thong tin sinh vien (1. IT) - (2. Biz): ");
+            int chon = 0;
+            chon = sc1.nextInt();
+            if (chon == 1) {
+                System.out.print("Nhap ho ten sinh vien IT: ");
+                String hotenIT = sc2.nextLine();
+                System.out.print("Nhap diem Java: ");
+                double diemJava = sc1.nextDouble();
+                System.out.print("Nhap diem Css: ");
+                double diemCss = sc1.nextDouble();
+                System.out.print("Nhap diem Html: ");
+                double diemHtml = sc1.nextDouble();
+                sv = new SinhVienIT(hotenIT, diemJava, diemCss, diemHtml);
+            } else if (chon == 2) {
+                System.out.print("Nhap ho ten sinh vien Biz: ");
+                String hotenBiz = sc2.nextLine();
+                System.out.print("Nhap diem Marketing: ");
+                double diemMarketing = sc1.nextDouble();
+                System.out.print("Nhap diem Sales: ");
+                double diemSales = sc1.nextDouble();
+                sv = new SinhVienBiz(hotenBiz, diemMarketing, diemSales);
+            }
+            if (sv != null) {
+                danhsachSinhVien.add(sv);
+            }
+            System.out.print("Ban co muon tiep tuc khong?(Yes(y)/No(n)): ");
+            tieptuc = sc2.nextLine();
+        } while (tieptuc.equalsIgnoreCase("y"));
+    }
+
+    private static void XuatDS() {
+        for (SinhVien sinhVien : danhsachSinhVien) {
+            sinhVien.xuat();
         }
-        for (SinhVienIT sinhVienIT : danhSachSinhVienIT) {
-            if(sinhVienIT.getDiem() < 9 && sinhVienIT.getDiem() >= 7.5){
-                sinhVienIT.xuat();
+    }
+
+    private static void XuatDSGioi() {
+        for (SinhVien sinhVien : danhsachSinhVien) {
+            if(sinhVien.getDiem() < 9 && sinhVien.getDiem() >= 7.5){
+                sinhVien.xuat();
             }
         }
     }
-    
+
+    private static void SapXepTheoDiem() {
+        
+    }
+
 }
